097b7cd9d8bf45fb7eae8f88b3885b6bbd6cc926
abdul wahid hashoory <whashoory@gmail.com>
2019-07-15 15:27:01 -0500

    added the BG

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,22 @@
 let projects = require("../models/HW&Projects.js");
 
 module.exports = function (app) {
+    // app.get("/", function (req, res) {
+    //     res.render("index");
+    //   });
 
   app.get("/portfolio", function (req, res) {
     res.render("portfolio", {projects});
   });
-
-  // If no matching route is found default to home
-  app.get("/contact", function (req, res) {
-    res.render("contact");
+  app.get("/about", function (req, res) {
+    res.render("about");
   });
 
+  app.get("/contact", function (req, res) {
+      res.render("contact");
+    });
+    
+    // If no matching route is found default to home
   app.get("*", function (req, res) {
     res.render("index");
   });
